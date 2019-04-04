4e546b49a811773dbab929f9738ca1b9b9e95a9e
peppelischio <41442122+peppelischio@users.noreply.github.com>
2019-04-04 23:45:05 +0200

    Modified UML class diagarm and flow diagrams (#20)
    * First draft of controller uml
    Added sequence diagrams of flow
    
    * Added model and view events
    Added Controller states but don't know how to manage the factory
    Added Scope flow in shoot cases
    Added VenomGranade flow in shoot cases
    Added an example of teleport and kinetic ray flow in move case
    Added view methods
    
    * Added planuml download script
    
    * Fixed plantuml_download.sh
    
    * Fixed plantuml_download.sh
    Added plantuml_run.sh
    
    * Fixed plantuml_download.sh & plantuml_run.sh
    
    * Added requested changes
    
    * Updated images

---
+++ plantuml_dowload.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed. Aborting."; exit 1; }
+command -v mvn >/dev/null 2>&1 || { echo >&2 "I require mvn but it's not installed. Aborting."; exit 1; }
+
+rm -rf plantuml_repo
+git clone git@github.com:plantuml/plantuml.git plantuml_repo
+cd plantuml_repo
+git checkout tags/v1.2019.4
+mvn package -DskipTests -Dmaven.javadoc.skip=true
+mv target/plantuml-1.2019.5-SNAPSHOT.jar ../plantuml.jar
+cd ..
+rm -rf plantuml_repo
+
+echo "plantuml.jar builded. Use it with java -jar plantuml.jar file1 file2 file3"
---
+++ plantuml_run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]
+  then
+    echo "Usage: plantuml_run [labN]"
+    exit
+fi
+
+if [ ! -f plantuml.jar ]; then
+    echo "plantuml.jar not found. Maybe you need to run plantuml_download script first"
+    exit
+fi
+
+command -v java >/dev/null 2>&1 || { echo >&2 "I require java but it's not installed. Aborting."; exit 1; }
+command -v convert >/dev/null 2>&1 || { echo >&2 "I require convert (from ImageMagick) but it's not installed. Aborting."; exit 1; }
+
+java -jar plantuml.jar -tsvg uml/uml.puml
+mkdir -p deliveries/$1
+convert uml/uml.svg deliveries/$1/uml.png
+mv uml/uml.svg deliveries/$1/uml.svg
+git add deliveries/$1/uml.png
+git add deliveries/$1/uml.svg
+
+echo "Completed!"
