2315a39176496ee5d2ece292459a616ecbbc8a5c
miranda96 <mirandamucignat@gmail.com>
2019-05-14 16:14:20 +0200

    change dashboard with only one player

--- src/main/java/com/adrenalinici/adrenaline/gui/controller/dasboardController.java
+++ src/main/java/com/adrenalinici/adrenaline/gui/controller/dasboardController.java
@@ -1,5 +1,15 @@
 package com.adrenalinici.adrenaline.gui.controller;
 
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 public class dasboardController {
 
+  @FXML
+  public void possibleMove(MouseEvent event){
+    //TODO
+  }
 }
