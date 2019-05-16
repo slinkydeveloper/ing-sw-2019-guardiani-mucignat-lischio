a2b69464fe6f0b246357c900229ec41cc1353f25
peppelischio <giuseppe.lischio@mail.polimi.it>
2019-05-16 14:54:42 +0200

    Implemented Tractor Beam. Not sure if changes in ChoosePlayersToHitFlowNode are totally legit

---
+++ src/test/java/com/adrenalinici/adrenaline/controller/guns/TractorBeamTest.java
@@ -0,0 +1,144 @@
+package com.adrenalinici.adrenaline.controller.guns;
+
+import com.adrenalinici.adrenaline.controller.DecoratedAlternativeEffectGun;
+import com.adrenalinici.adrenaline.controller.GunLoader;
+import com.adrenalinici.adrenaline.controller.nodes.guns.*;
+import com.adrenalinici.adrenaline.flow.FlowNode;
+import com.adrenalinici.adrenaline.model.common.AmmoColor;
+import com.adrenalinici.adrenaline.model.common.PlayerColor;
+import com.adrenalinici.adrenaline.model.common.Position;
+import com.adrenalinici.adrenaline.model.event.ModelEvent;
+import com.adrenalinici.adrenaline.model.fat.PlayerDashboard;
+import com.adrenalinici.adrenaline.view.event.AlternativeGunEffectChosenEvent;
+import com.adrenalinici.adrenaline.view.event.EnemyMovementChosenEvent;
+import com.adrenalinici.adrenaline.view.event.PlayerChosenEvent;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static com.adrenalinici.adrenaline.testutil.MyConditions.isDashboardCellUpdatedEvent;
+import static com.adrenalinici.adrenaline.testutil.MyConditions.isPlayerDashboardUpdateEvent;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TractorBeamTest extends BaseGunTest {
+  @Override
+  protected List<FlowNode> nodes() {
+    return Arrays.asList(
+      new ChooseAlternativeEffectForGunFlowNode(),
+      new ChoosePlayersToHitFlowNode(),
+      new GunChooseEnemyMovementFlowNode(2)
+    );
+  }
+
+  @Override
+  protected String gunId() {
+    return "tractor_beam";
+  }
+
+  @Test
+  public void testBaseEffect() {
+    context.setTurnOfPlayer(PlayerColor.GREEN);
+
+    model.getDashboard().getDashboardCell(Position.of(0, 0)).addPlayer(PlayerColor.GREEN);
+    model.getDashboard().getDashboardCell(Position.of(0, 2)).addPlayer(PlayerColor.GRAY);
+    model.getDashboard().getDashboardCell(Position.of(0, 2)).addPlayer(PlayerColor.YELLOW);
+    model.getDashboard().getDashboardCell(Position.of(2, 2)).addPlayer(PlayerColor.CYAN);
+
+    PlayerDashboard killerPlayerDashboard = model.getPlayerDashboard(PlayerColor.GREEN);
+    killerPlayerDashboard.addGun(gunId());
+
+    List<ModelEvent> receivedModelEvents = new ArrayList<>();
+    model.registerObserver(receivedModelEvents::add);
+
+    context.nextPhase(
+      viewMock,
+      new AlternativeEffectGunFlowStateImpl((DecoratedAlternativeEffectGun) GunLoader.INSTANCE.getDecoratedGun(gunId()))
+    );
+    context.handleEvent(new AlternativeGunEffectChosenEvent(false), viewMock);
+    context.handleEvent(new PlayerChosenEvent(PlayerColor.GRAY), viewMock);
+
+    context.handleEvent(
+      new EnemyMovementChosenEvent(Position.of(0, 0), PlayerColor.GRAY),
+      viewMock
+    );
+
+    assertThat(model.getPlayerPosition(PlayerColor.GRAY))
+      .isEqualTo(Position.of(0, 0));
+
+    assertThat(receivedModelEvents)
+      .haveExactly(1, isDashboardCellUpdatedEvent(0, 2))
+      .haveExactly(1, isDashboardCellUpdatedEvent(0, 0));
+
+    assertThat(model.getPlayerDashboard(PlayerColor.GRAY).getDamages())
+      .containsOnly(PlayerColor.GREEN);
+
+    assertThat(context.getKilledPlayers().isEmpty()).isTrue();
+
+    assertThat(receivedModelEvents)
+      .haveExactly(1, isPlayerDashboardUpdateEvent(PlayerColor.GRAY));
+
+    assertThat(killerPlayerDashboard.getLoadedGuns())
+      .doesNotContain(gunId());
+
+    assertThat(killerPlayerDashboard.getUnloadedGuns())
+      .contains(gunId());
+
+    assertThat(killerPlayerDashboard.getAmmos())
+      .containsExactlyInAnyOrder(AmmoColor.YELLOW, AmmoColor.BLUE, AmmoColor.RED);
+  }
+
+  @Test
+  public void testPunisherEffect() {
+    context.setTurnOfPlayer(PlayerColor.GREEN);
+
+    model.getDashboard().getDashboardCell(Position.of(0, 1)).addPlayer(PlayerColor.GREEN);
+    model.getDashboard().getDashboardCell(Position.of(1, 0)).addPlayer(PlayerColor.GRAY);
+    model.getDashboard().getDashboardCell(Position.of(0, 2)).addPlayer(PlayerColor.YELLOW);
+    model.getDashboard().getDashboardCell(Position.of(2, 2)).addPlayer(PlayerColor.CYAN);
+
+    PlayerDashboard killerPlayerDashboard = model.getPlayerDashboard(PlayerColor.GREEN);
+    killerPlayerDashboard.addGun(gunId());
+
+    List<ModelEvent> receivedModelEvents = new ArrayList<>();
+    model.registerObserver(receivedModelEvents::add);
+
+    context.nextPhase(
+      viewMock,
+      new AlternativeEffectGunFlowStateImpl((DecoratedAlternativeEffectGun) GunLoader.INSTANCE.getDecoratedGun(gunId()))
+    );
+    context.handleEvent(new AlternativeGunEffectChosenEvent(true), viewMock);
+    context.handleEvent(new PlayerChosenEvent(PlayerColor.GRAY), viewMock);
+
+    assertThat(model.getPlayerDashboard(PlayerColor.GRAY).getDamages())
+      .isEqualTo(Collections.nCopies(3, PlayerColor.GREEN));
+
+    assertThat(context.getKilledPlayers().isEmpty()).isTrue();
+
+    assertThat(receivedModelEvents)
+      .haveExactly(1, isPlayerDashboardUpdateEvent(PlayerColor.GRAY));
+
+    context.handleEvent(
+      new EnemyMovementChosenEvent(model.getPlayerPosition(PlayerColor.GREEN), PlayerColor.GRAY),
+      viewMock
+    );
+
+    assertThat(model.getPlayerPosition(PlayerColor.GRAY))
+      .isEqualTo(Position.of(0, 1));
+
+    assertThat(receivedModelEvents)
+      .haveExactly(1, isDashboardCellUpdatedEvent(1, 0))
+      .haveExactly(1, isDashboardCellUpdatedEvent(0, 1));
+
+    assertThat(killerPlayerDashboard.getLoadedGuns())
+      .doesNotContain(gunId());
+
+    assertThat(killerPlayerDashboard.getUnloadedGuns())
+      .contains(gunId());
+
+    assertThat(killerPlayerDashboard.getAmmos())
+      .containsOnly(AmmoColor.BLUE);
+  }
+}
