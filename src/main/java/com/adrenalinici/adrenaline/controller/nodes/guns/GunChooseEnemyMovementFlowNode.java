6e91ce5840e12c0eda9afbfba5efb2150565b545
peppelischio <giuseppe.lischio@mail.polimi.it>
2019-05-19 22:28:59 +0200

    Done required changes

--- src/main/java/com/adrenalinici/adrenaline/controller/nodes/guns/GunChooseEnemyMovementFlowNode.java
+++ src/main/java/com/adrenalinici/adrenaline/controller/nodes/guns/GunChooseEnemyMovementFlowNode.java
@@ -5,17 +5,29 @@ import com.adrenalinici.adrenaline.flow.FlowState;
 import com.adrenalinici.adrenaline.model.common.PlayerColor;
 import com.adrenalinici.adrenaline.model.common.Position;
 import com.adrenalinici.adrenaline.model.fat.GameModel;
+import com.adrenalinici.adrenaline.util.JsonUtils;
+import com.adrenalinici.adrenaline.util.TriPredicate;
 import com.adrenalinici.adrenaline.view.GameView;
 import com.adrenalinici.adrenaline.view.event.ViewEvent;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.adrenalinici.adrenaline.controller.nodes.ControllerNodes.gunEnemyMovement;
 
+/**
+ * This node manages the movement of an enemy.
+ * Before showing the player all the positions in which he can move the enemy,
+ * it checks the "mode" config in gun's json in order to select only a certain
+ * set of available positions in accordance with this.
+ */
 public class GunChooseEnemyMovementFlowNode implements SkippableGunFlowNode<GunFlowState> {
   int distance;
+  public static final String ONLY_ONE_DIRECTION = "only_one_direction";
+  public static final String ONLY_VISIBLE_SQUARES = "only_visible_squares";
+  public static final String DEFAULT = "default";
 
   public GunChooseEnemyMovementFlowNode(int distance) {
     this.distance = distance;
@@ -29,18 +41,18 @@ public class GunChooseEnemyMovementFlowNode implements SkippableGunFlowNode<GunF
 
   @Override
   public void onJump(GunFlowState flowState, GameView view, GameModel model, ControllerFlowContext context) {
-    PlayerColor enemy = new ArrayList<>(flowState.getHitPlayers().keySet()).get(0);
+    PlayerColor enemy = flowState.getChosenPlayersToHit().get(0);
     Position actualEnemyPosition = model.getPlayerPosition(enemy);
-    if (resolveMovementModeConfiguration(flowState).equals("only one direction")) {
+    if (resolveMovementModeConfiguration(flowState).equals(ONLY_ONE_DIRECTION)) {
       view.showAvailableMovements(
         model.getDashboard().calculateMovementsInOneDirection(actualEnemyPosition, distance)
       );
-    } else if (resolveMovementModeConfiguration(flowState).equals("only visible squares")) {
+    } else if (resolveMovementModeConfiguration(flowState).equals(ONLY_VISIBLE_SQUARES)) {
       view.showAvailableMovements(
         model.getDashboard().calculateMovements(actualEnemyPosition, distance)
           .stream()
-          .filter(enemyPosition ->
-            model.getDashboard().calculateIfVisible(enemyPosition, model.getPlayerPosition(context.getTurnOfPlayer()))
+          .filter(possibleEnemyPosition ->
+            model.getDashboard().calculateIfVisible(possibleEnemyPosition, model.getPlayerPosition(context.getTurnOfPlayer()))
           )
           .collect(Collectors.toList())
       );
@@ -67,6 +79,9 @@ public class GunChooseEnemyMovementFlowNode implements SkippableGunFlowNode<GunF
   }
 
   private String resolveMovementModeConfiguration(GunFlowState flowState) {
+    if (!flowState.resolvePhaseConfiguration(id()).has("mode"))
+      return DEFAULT;
+
     return flowState.resolvePhaseConfiguration(id()).get("mode").asText();
   }
 }
