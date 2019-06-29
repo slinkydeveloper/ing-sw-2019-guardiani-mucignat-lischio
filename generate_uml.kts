ca68cca7f92987a6f4174d448a2b2027acc2be0b
slinkydeveloper <francescoguard@gmail.com>
2019-06-29 17:13:03 +0200

    Updated pom versions

--- generate_uml.kts
+++ generate_uml.kts
@@ -1,12 +1,36 @@
 #!/usr/bin/env kscript
 
 @file:DependsOn("ch.ifocusit:plantuml-builder:1.4")
-@file:DependsOn("com.adrenalinici:adrenaline-common:1.0-SNAPSHOT")
-@file:DependsOn("com.adrenalinici:adrenaline-client:1.0-SNAPSHOT")
-@file:DependsOn("com.adrenalinici:adrenaline-gui:1.0-SNAPSHOT")
-@file:DependsOn("com.adrenalinici:adrenaline-cli:1.0-SNAPSHOT")
-@file:DependsOn("com.adrenalinici:adrenaline-server:1.0-SNAPSHOT")
+@file:DependsOn("com.adrenalinici:adrenalina-common:1.0-SNAPSHOT")
+@file:DependsOn("com.adrenalinici:adrenalina-client:1.0-SNAPSHOT")
+@file:DependsOn("com.adrenalinici:adrenalina-gui:1.0-SNAPSHOT")
+@file:DependsOn("com.adrenalinici:adrenalina-cli:1.0-SNAPSHOT")
+@file:DependsOn("com.adrenalinici:adrenalina-server:1.0-SNAPSHOT")
 
-fun renderUML(mainPackage: String): String {
+import java.io.File
 
+fun renderUML(vararg packages: String): String {
+  val builder = ClassDiagramBuilder()
+
+  packages.forEach {pkgName ->
+    val classPath = ClassPath.from(ClassLoader.getSystemClassLoader())
+
+    com.adrenalinici.adrenaline.common.model.Action.SHOOT
+
+    classPath.getTopLevelClasses(pkgName).forEach {
+      println("Discovered ${it.load()}")
+      builder.addClasse(it.load())
+    }
+    Package.getPackages().forEach { println(it.name) }
+    val pkg = Package.getPackage(pkgName)
+    println("Adding package ${pkg.name}")
+    builder
+      .addPackage(pkg)
+  }
+
+  return builder.build()
 }
+
+File("stuff.puml").printWriter().use { out -> out.print(renderUML(
+  "com.adrenalinici.adrenaline.common.model"
+)) }
