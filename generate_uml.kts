8d7b331104d939ebb2f1de7f9e96dc38641d51d7
slinkydeveloper <francescoguard@gmail.com>
2019-06-29 14:03:06 +0200

    Trying to figure out some magic for uml generation

---
+++ generate_uml.kts
@@ -0,0 +1,12 @@
+#!/usr/bin/env kscript
+
+@file:DependsOn("ch.ifocusit:plantuml-builder:1.4")
+@file:DependsOn("com.adrenalinici:adrenaline-common:1.0-SNAPSHOT")
+@file:DependsOn("com.adrenalinici:adrenaline-client:1.0-SNAPSHOT")
+@file:DependsOn("com.adrenalinici:adrenaline-gui:1.0-SNAPSHOT")
+@file:DependsOn("com.adrenalinici:adrenaline-cli:1.0-SNAPSHOT")
+@file:DependsOn("com.adrenalinici:adrenaline-server:1.0-SNAPSHOT")
+
+fun renderUML(mainPackage: String): String {
+
+}
